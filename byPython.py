fc1725cbf8114f6667ccee390dd0a98ce4c1d731
joeyma <hi@mintiny.com>
2023-03-21 12:23:08 +0800

    JQuery下的AJAX调用接口方法

--- byPython.py
+++ byPython.py
@@ -1,4 +1,5 @@
 import requests
+# 此处仅提供一个简单的示例，具体实现起来方案有很多，可按需开发
 def python_demo():
     url = '你的接口地址'
     files = {'fileArray': ('demo.jpg', open('demo.jpg', 'rb'), "image/jpeg")}
@@ -13,8 +14,11 @@ python_demo()
 
 # 以下代码只是调用结果展示，复制代码后删掉或者注释掉下面内容才能运行
 # 响应结果
-{"result":"success",
- "url":"返回的文件链接",
- "originFileName":"原文件名称",
- "lessurl":"不含https://前缀的链接"
+{
+    "status":"success",
+    "statusCode":"200",
+    "resultData":"success：File uploaded",
+    "url":"返回的文件链接",
+    "originFileName":"原文件名称",
+    "lessurl":"不含https://前缀的链接"
 }
